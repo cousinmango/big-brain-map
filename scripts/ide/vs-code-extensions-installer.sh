5be6c91c7484e6bb6a7baa7c7b55c23f3d172ff3
slimlime <30815731+slimlime@users.noreply.github.com>
2020-12-05 06:22:05 +1000

    chore(ide): :fire: Remove unused extensions from script
    Some framework-specific and database extensions were present during script run
    
    #7

--- scripts/ide/vs-code-extensions-installer.sh
+++ scripts/ide/vs-code-extensions-installer.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 # Generated using `code --list-extensions | xargs -L 1 echo code --install-extension`
 
-# code --install-extension 1tontech.angular-material
-# code --install-extension aaron-bond.better-comments
-# code --install-extension alexiv.vscode-angular2-files
 code --install-extension alexkrechik.cucumberautocomplete
-# code --install-extension Angular.ng-template
-# code --install-extension apollographql.vscode-apollo
-# code --install-extension atlassian.atlascode
-# code --install-extension bajdzis.vscode-database
 code --install-extension ChakrounAnas.turbo-console-log
 code --install-extension christian-kohler.path-intellisense
-# code --install-extension ckolkman.vscode-postgres
 code --install-extension cmstead.jsrefactor
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension cyrilletuzi.angular-schematics
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
-# code --install-extension deerawan.vscode-protractor-snippets
 code --install-extension donjayamanne.githistory
 code --install-extension DotJoshJohnson.xml
 code --install-extension eamodio.gitlens
@@ -29,57 +20,32 @@ code --install-extension firefox-devtools.vscode-firefox-debug
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension frasercrosbie.typescript-interface-to-builder-class
-# code --install-extension gabduss.run-protractor
 code --install-extension GitHub.vscode-pull-request-github
-# code --install-extension GraphQL.vscode-graphql
 code --install-extension GregorBiswanger.json2ts
 code --install-extension Gruntfuggly.todo-tree
 code --install-extension hbenl.vscode-test-explorer
-# code --install-extension infinity1207.angular2-switcher
-# code --install-extension johnpapa.angular-essentials
-# code --install-extension johnpapa.Angular2
 code --install-extension johnpapa.vscode-peacock
 code --install-extension johnpapa.winteriscoming
 code --install-extension krizzdewizz.refactorix
-# code --install-extension loiane.angular-extension-pack
-# code --install-extension luciannaie.protractor-test-runner
-# code --install-extension MarinhoBrandao.Angular2Tests
 code --install-extension mehmetbsrn.test-generator
-# code --install-extension Mikael.Angular-BeastCode
-# code --install-extension ms-azuretools.vscode-docker
-# code --install-extension ms-mssql.mssql
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode.typescript-javascript-grammar
 code --install-extension ms-vscode.vscode-typescript-next
-# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension msjsdiag.debugger-for-edge
 code --install-extension mtxr.sqltools
-# code --install-extension NG-42.ng-fortytwo-vscode-extension
-# code --install-extension ng-refactor.ng-refactor
 code --install-extension nhoizey.gremlins
 code --install-extension nicoespeon.abracadabra
-# code --install-extension nrwl.angular-console
-# code --install-extension obenjiro.arrr
-# code --install-extension PKief.material-icon-theme
-# code --install-extension planbcoding.vscode-react-refactor
 code --install-extension quicktype.quicktype
-# code --install-extension raagh.angular-karma-test-explorer
 code --install-extension rbbit.typescript-hero
-# code --install-extension rctay.karma-problem-matcher
 code --install-extension redhat.vscode-xml
 code --install-extension redhat.vscode-yaml
-# code --install-extension rintoj.chai-spec-generator
-# code --install-extension run-at-scale.terraform-doc-snippets
 code --install-extension salbert.comment-ts
-# code --install-extension SimonTest.simontest
 code --install-extension steoates.autoimport
 code --install-extension stevejpurves.cucumber
 code --install-extension stringham.move-ts
-# code --install-extension UVBrain.Angular2
-# code --install-extension xabikos.JasmineSnippets
 code --install-extension xletohic.cucumber-json-formatter
 code --install-extension ypresto.vscode-intelli-refactor
