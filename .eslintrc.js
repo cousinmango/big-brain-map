1d850c26d790d1be8ede50558552e892d7d396cd
seant1 <sean.tran@outlook.com.au>
2020-12-09 17:35:16 +1100

    chore(ide): initial :construction: WIP linting configs
    Co-authored-by: sl<30815731+slimlime@users.noreply.github.com>
    Co-authored-by: seant1 <45996296+seant1@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    amd: true,
+    node: true,
+  },
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "quote-props": ["error", "as-needed"],
+    "no-var": 2,
+    "no-const-assign": "error",
+    radix: "error",
+    "prefer-template": "error",
+    "prefer-const": "error",
+    "prefer-spread": "error",
+    eqeqeq: ["error", "always"],
+    "default-case": 2,
+
+    "newline-before-return": "error",
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: false, variables: true },
+    ],
+  },
+};
