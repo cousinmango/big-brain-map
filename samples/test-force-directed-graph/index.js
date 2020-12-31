a3e6873e4abd56655720bedc8f7f8e5e9cc27b1b
S <SL@SL>
2020-12-31 19:46:57 +1000

    style(force-directed-graph, eslintrc): lint autofix prettier eslint jsdocs
    Lots of fixes. Classes. Types
    
    BREAKING CHANGE: Something broke. Maybe
    
    Co-authored-by: sl <30815731+slimlime@users.noreply.github.com>
    Co-authored-by: seant1 <45996296+seant1@users.noreply.github.com>

--- samples/test-force-directed-graph/index.js
+++ samples/test-force-directed-graph/index.js
@@ -1,5 +1,5 @@
 /*
-- FIXME: Remember to enable this later 
+- FIXME: Remember to enable this later
 // @ts-check
 */
 /* global d3 */
@@ -65,7 +65,7 @@ function getDragBehaviour(simulation) {
 const height = window.innerHeight;
 const width = window.innerWidth;
 /**
- * Scales the data node .group value into arbitrary hexadecimal strings 
+ * Scales the data node .group value into arbitrary hexadecimal strings
  * (for colour values)
  * e.g 1 = #00000 2 = #1f1f1f 10 = #ffffff
  * @return {d3.ValueFn<
@@ -83,7 +83,7 @@ function color() {
 
 
 /**
- * Does something to the selection using 
+ * Does something to the selection using
  * (should be triggered on the zoom event)
  * @param {d3.Selection<
  *  SVGSVGElement, any, HTMLElement, any
@@ -109,7 +109,7 @@ function chart(data) {
   // Mutato potato
   // Any number here as it is unused, overridden by the node-radius function
   const collisionation = d3.forceCollide(0);
-  const raddddd = collisionation.radius(({ id }) => id.length * 5)
+  const raddddd = collisionation.radius(({ id }) => id.length * 5);
 
   const simulation = d3
     .forceSimulation(nodes)
@@ -118,7 +118,7 @@ function chart(data) {
       d3
         .forceLink(links)
         .id((d) => d.id)
-        .distance(1)
+        .distance(1),
 
     )
     .force("charge", d3.forceManyBody().strength(-500))
@@ -209,15 +209,14 @@ function chart(data) {
   });
 
   /**
-   * @param event
-   * @param d
+   * @param {Selection<SVGEvent> }event
+   * @param {Datum} d
    */
   function clicked(event, d) {
-
     /**
      * @type {string}
      */
-    const escapedID = '#' + CSS.escape(d.id);
+    const escapedID = `#${CSS.escape(d.id)}`;
 
     console.log(`clicked: ${d3.select(escapedID).attr("id")}`);
     const translate = [width / 2, height / 2];
@@ -233,96 +232,127 @@ function chart(data) {
           ,
           translate[1] -
           d3.select(escapedID)
-            .attr("cy")
-        )
+            .attr("cy"),
+        ),
       ); // updated for d3 v4
   }
 
   return svg.node();
 }
 
+/* eslint-disable no-unused-vars */
 
 /**
  * Node definition
  * @example {id: 1, group: 1}
- * 
- * - TODO: Move these to another file and use some jsdoc comments to find the path resolution of type???? or make a @types file
- * Nodes positions are defined dynamically through physic simulation downstream with relation to link strength
+ *
+ * - TODO: Move these to another file and use some jsdoc comments to find
+ * the path resolution of type???? or make a @types file
+ * Nodes positions are defined dynamically through physic simulation
+ * downstream with relation to link strength
  */
 class HappyNode {
   /**
+   *
+   * @param {string} id
+   * @param {string} group
+   */
+  constructor(id, group) {
+    // class members. Should be private.
+    /**
+     * @private
+     * @type {string}
+     **/
+    this.id = id;
+    this.group = group;
+  }
+  /**
    * @member
-   * @type {string} 
+   * @type {string}
    * naive key
-   * 
+   *
    * The name of the node
    * Also should be unique
-   * This will break if someone wants to add another node 
-   * 
-   * - TODO: This should eventually be migrated to a standard unique ID integer or UUID
+   * This will break if someone wants to add another node
+   *
+   * - TODO: This should eventually be migrated to a standard unique ID integer
+   *  or UUID
    */
-  id;
 
   /**
-   * @type {number} 
-   * 
+   * @type {number}
+   *
    * Group number
    *  Also being used as naive key
    *  Colour groupings
    */
-  group;
 }
-
 /**
  * Link definition for plotting D3 strokes
- * The positive strength of `value` determines how nodes are positioned in relation to each other
+ * The positive strength of `value` determines how nodes are positioned in
+ * relation to each other
  * when warmed up in the physics simulation
- * 
+ *
  */
 class HappyLink {
+  /**
+   * The id name of the linked source node
+   * @member
+   * @type {string}
+   **/
+  /**
+   * The target that the source is linked to
+   * @member {string}
+   * @type {string}
+   */
+
+  /**
+   *
+   * @param {string} source
+   * @param {string} target
+   * @param {number} value
+   */
+  constructor(source, target, value) {
+    // class members. Should be private.
     /**
-     * The id name of the linked source node
-     * @member
+     * @private
      * @type {string}
      **/
-    source;
-    /**
-     * The target that the source is linked to
-     * @member {string}
-     * @type {string}
-     */
-    target;
-
+    this.source = source;
+    this.target = target;
     /**
      * The strength of the relationship.
      * @member {number}
-     * @type {number} 
-     * 
-     */
-    value;
-}
-/** @type {HappyNode} */
-
-class MiserableNodesLinks {
-  
-  /**
-   * @member {number}
-   */
-
-  /**
-   * constructor description
-   * @param  {[number} config [description]
-   */
-  constructor(config) {
-    // class members. Should be private. 
-    /** @private 
      * @type {number}
-     **/
-    this.nodes = config;
-    /** @private */
-    this.links = "bananas";
+     *
+     */
+    this.value = value;
   }
 }
+
+/**
+ *
+ */
+class MiserableNodesLinks {
+  /**
+   *
+   * @param {HappyNode[]} nodes
+   * @param {HappyLink[]} links
+   */
+  constructor(nodes, links) {
+    // class members. Should be private.
+    /**
+     * @private
+     * @type {number}
+     **/
+    this.nodes = nodes;
+    /** @private */
+    this.links = links;
+  }
+}
+/* eslint-enable no-unused-vars */
+
+
 /**
  * @param {{
  *  nodes: [
@@ -335,14 +365,14 @@ class MiserableNodesLinks {
  *    {
  *      source: string,
  *      target: string,
- *      value: string,   
+ *      value: string,
  *    }
  *  ]
  * }} theMiserableDataNodesLinks Assumed not undefined null
- * 
+ *
  * @param {HappyNode} newNode
  * @param {HappyLink} newLink
- * 
+ *
  * @return {{
  *  nodes: [
   *    {
@@ -354,7 +384,7 @@ class MiserableNodesLinks {
   *    {
   *      source: string,
   *      target: string,
-  *      value: string,   
+  *      value: string,
   *    }
   *  ]
   * }} the new object with nodes array and links array properties
@@ -362,16 +392,15 @@ class MiserableNodesLinks {
 function addSomeJsonNode(theMiserableDataNodesLinks, newNode, newLink) {
   theMiserableDataNodesLinks.nodes.push(newNode);
   theMiserableDataNodesLinks.links.push(newLink);
-  
+
   return theMiserableDataNodesLinks;
 }
 
 
-
 /** @type {HappyNode} */
-const newNode = {id: "id", group: 1};
+const newNode = { id: "id", group: 1 };
 /** @type {HappyLink} */
-const newLink = {source: "Combeferre", target: "id", value: 1000};
+const newLink = { source: "Combeferre", target: "id", value: 1000 };
 
 getData()
   .then((data) => addSomeJsonNode(data, newNode, newLink))
@@ -379,17 +408,20 @@ getData()
   .catch((reason) => console.error(`Something went horribly wrong`, reason));
 
 
-/* 
-  var mutableCounterOhNo = 1;
+let mutableCounterOhNo = 1;
 
-  const recurseThisForeverTestAddStuffChart = setInterval(() => {
-    getData()
-    .then((data) => { 
+const recurseThisForeverTestAddStuffChart = setInterval(() => {
+  getData()
+    .then((data) => {
+      const someNumberOfElementsToDoThings = Array.from({
+        length: mutableCounterOhNo,
+      });
 
-      const someNumberOfElementsToDoThings = Array.from({length: mutableCounterOhNo});
-
-      someNumberOfElementsToDoThings.forEach(() => addSomeJsonNode(data, newNode, newLink));
-
+      someNumberOfElementsToDoThings.forEach(() => addSomeJsonNode(
+        data,
+        newNode,
+        newLink,
+      ));
 
       mutableCounterOhNo += 1;
 
@@ -397,7 +429,7 @@ getData()
     })
     .then(chart)
     .catch((reason) => console.error(`Something went horribly wrong`, reason));
-  },
-  2000
-  )
-*/
+}, 2000,
+);
+
+recurseThisForeverTestAddStuffChart();
