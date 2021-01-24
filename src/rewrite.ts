4e585f75811b3eb3286d949f911d4d500396e3e3
ml <30815731+slimlime@users.noreply.github.com>
2021-01-24 10:32:46 +1000

    feat(rewrite): json import seems to work

--- src/rewrite.ts
+++ src/rewrite.ts
@@ -11,8 +11,16 @@ import type {
   Transition,
   Selection,
 } from 'd3';
+
+import type * as gg from 'd3';
 import { MiserableNodesLinks } from './models/miserable-nodes-links';
 import { HappyLink } from './models/happy-link';
 import { HappyNode } from './models/happy-node';
+import sampleMiserablesDataJson from './models/miserables.json';
 
-const d3 = window.d3;
+const d3: typeof gg = window.d3;
+
+// TypeScript language server actually does a good job inferring types directly from json file
+// add type anyway
+const sampleData: MiserableNodesLinks = sampleMiserablesDataJson;
+
