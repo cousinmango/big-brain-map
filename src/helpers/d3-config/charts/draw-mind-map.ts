9ce50dae317e209e84f4d916b08343f6c02f0753
ml <30815731+slimlime@users.noreply.github.com>
2021-02-07 10:56:23 +1000

    feat(draw-mind-map): add working pan transform zoom on click
    Selection vs Transition typings is still broken. D3 maintainers.. But it works!
    
    BREAKING CHANGE: Typings do not work even though this is intended functionality

--- src/helpers/d3-config/charts/draw-mind-map.ts
+++ src/helpers/d3-config/charts/draw-mind-map.ts
@@ -95,23 +95,90 @@ export function drawChartFromData(
     .call(getDragBehaviourConfigForSelectionCall(forceSim, d3))
     .on(
       'click',
-      (_event: d.D3ZoomEvent<Element, unknown>, _d): d.ZoomBehavior<Element, unknown> => {
-        return (
-          d3
-            .zoom()
-            .extent([
-              [0, 0],
-              [innerWidth, innerHeight],
-            ])
-            .scaleExtent([0.1, 8])
-            // "start", "zoom", "end"
-            .on('zoom', (zoomEvent: d.D3ZoomEvent<Element, BrainNodeDatum>, _node: unknown) => {
-              const { transform } = zoomEvent;
+      (zoomEvent, _node) => {
+        // Do we really need to pass in the node
+        // Just to get the node id
+        // and then mutate/select the DOM based on the passed in node ID?
+        // Can we pass in just the selection instead
 
-              svgContainerGroupG.attr('transform', transform.toString());
-            })
+        // We can access the attributes directly...
+        // Doesn't need another d3.select each time.
+        // The examples have a mishmash of styles and conventions
+
+        // Benefit of typings.
+        // Can see the properties available conforming to the library functions
+        // - TODO: Confirm the d3 documentation sim sets the x y similarly to cx cy
+        const nodeX = _node.x ?? 0;
+        const nodeY = _node.y ?? 0;
+        const width = innerWidth;
+        const height = innerHeight;
+        const viewCentreX = width / 2;
+        const viewCentreY = height / 2;
+
+        const selectedWholeViewboxToRezoomTransformPan: ParentSvgGroupSelectionForWholeBrainMap = viewboxedParentSvg;
+
+        /**
+         * Pan zoom the whole rendered SVG
+         */
+        type TopLevelPanZoomTransition = d.Transition<SVGSVGElement, unknown, HTMLElement, unknown>;
+
+        const baseTransform = d3.zoomIdentity;
+        // No datum model for typing here. General top-level SVG and HTML
+        const panZoomTransitionToBuild: TopLevelPanZoomTransition = selectedWholeViewboxToRezoomTransformPan.transition();
+        const transitionDurationMilliseconds = 750;
+
+        const translationX = viewCentreX - nodeX;
+        const translationY = viewCentreY - nodeY;
+
+        const translatedTransform: d.ZoomTransform = baseTransform.translate(
+          translationX,
+          translationY,
         );
+
+   
+
+        const zoomTransform = d3
+          .zoom()
+          .extent([
+            [0, 0],
+            [width, height],
+          ])
+          .scaleExtent([0.1, 8])
+          .on('zoom', (zoomEvent, _node) => {
+            // The end state of the zoom which would potentially be a reusable function
+            const parentGGroupToTransform = svgContainerGroupG;
+            const transform = zoomEvent.transform;
+            parentGGroupToTransform.attr('transform', transform);
+          });
+
+
+          const builtPanZoomTransition: TopLevelPanZoomTransition = panZoomTransitionToBuild
+          .duration(transitionDurationMilliseconds)
+          .call(zoomTransform.transform, translatedTransform);
+
       },
+      // (_event: d.D3ZoomEvent<Element, unknown>, _d): d.ZoomBehavior<Element, unknown> => {
+
+      // return (
+      //   d3
+      //     .zoom()
+      //     .extent([
+      //       [0, 0],
+      //       [innerWidth, innerHeight],
+      //     ])
+      //     .scaleExtent([0.1, 8])
+      //     // "start", "zoom", "end"
+      //     .on(
+      //       'zoom',
+      //       (
+      //         zoomEvent: d.D3ZoomEvent<Element, BrainNodeDatum>,
+      //         _node: unknown | BrainNodeDatum,
+      //       ) => {
+      //         const { transform } = zoomEvent;
+
+      //         svgContainerGroupG.attr('transform', transform.toString());
+      //       },
+      //     )
     );
 
   const paintedLinks = getSelectedJoinedStrokeLinks(svgContainerGroupG, links);
